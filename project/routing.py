6cbfdb54bcf3af1535f9fde8d4b72a0d4495b078
marian <marian@MacBook-Pro-Mac.local>
2018-05-01 18:27:47 +0300

    clear all

--- project/routing.py
+++
@@ -1,8 +0,0 @@
-from channels.routing import route
-from messenger.consumer import connect, receive, disconnect
-
-channel_routing = [
-    route("websocket.connect", connect),
-    route("websocket.receive", receive),
-    route("websocket.disconnect", disconnect),
-]
